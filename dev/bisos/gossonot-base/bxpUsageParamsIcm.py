e8d32cbf6a4ec4ae0f1f32335b3a14ce31f38eb0
Mohsen-Bisp0017 BANAN <form@mohsen.banan.1.byname.net>
2018-09-30 21:20:41 -0700

    Initial imports.

---
+++ dev/bisos/gossonot-base/bxpUsageParamsIcm.py
@@ -0,0 +1,6 @@
+
+from bisos.things import toIcm
+
+tpl = toIcm.Target_Param_List()
+tpl.parameterAdd(parameterType="bxp", base="/de/bx/current/district/librecenter/tiimi/targets/bxp/paramInfo/kpi/cpu/nuOfProcesses", dnType="path")
+tpl.parameterAdd(parameterType="bxp", base="/de/bx/current/district/librecenter/tiimi/targets/bxp/paramInfo/kpi/cpu/loadavg", dnType="path")
